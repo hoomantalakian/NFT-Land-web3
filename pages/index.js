76818cc780313afd9001469546a07f5efd9bd5c1
Hooman Talakian <hooman.talakian@gmail.com>
2022-11-16 23:36:46 +0330

    some basic style is added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Head from "next/head";
-import { Fragment } from "react";
-
+import NFTCard from "../components/NFTCard.jsx";
+import { useState } from "react";
+// ==================================
 export default function Home() {
+	const [address, setAddress] = useState("");
+	const [data, setData] = useState([]);
+
+	const fetchNFTs = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch(`/api/get-nfts?wallet=${address}`);
+			if (!response.ok) return alert("Something went wrong!");
+			const data = await response.json();
+			console.log(data);
+			if (data.data.totalCount == 0) return alert("Wallet has no NFTs");
+			setData(data.data.ownedNfts);
+		} catch (err) {
+			console.error(err);
+			alert("There was an error fetching NFTs!");
+		}
+	};
 	return (
-		<div>
+		<div className="container text-center m-auto">
 			<Head>
 				<title>NFT Land</title>
 				<meta
@@ -12,9 +30,29 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Fragment>
-				<h1 className="text-3xl font-bold underline">Hello!</h1>
-			</Fragment>
+			<search-box>
+				<h1 className="text-center font-bold">NFT Land</h1>
+				<form className="flex flex-col mt-10">
+					<input
+						value={address}
+						onChange={(e) => setAddress(e.target.value)}
+						type="text"
+						placeholder="Wallet Address"
+						className="pl-2 w-3/5 self-center"
+					/>
+					<button
+						onClick={fetchNFTs}
+						className="border w-1/5 self-center mt-5 rounded-md"
+					>
+						Find NFTs !
+					</button>
+				</form>
+			</search-box>
+			<div className="grid grid-cols-3 gap-5 mt-10">
+				{data.map((nft) => (
+					<NFTCard key={nft.tokenId} data={nft}></NFTCard>
+				))}
+			</div>
 		</div>
 	);
 }
