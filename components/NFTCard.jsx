bcec36a850a1bcdd459785a4291cc5210a3a860c
Hooman Talakian <hooman.talakian@gmail.com>
2022-11-17 00:34:53 +0330

    temporary card layout

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,7 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 function NFTCard({ data }) {
-	return <div>NFT-Card</div>;
+	return (
+		<div className="flex flex-col p-5 border rounded-lg">
+			<img
+				src={data?.media[0]?.gateway}
+				alt={data?.description || "There is no alt text"}
+			/>
+			<div className="mt-2">{data.title || <p>No Name!</p>}</div>
+			<div className="mt-2">{data.contract.address}</div>
+			<div className="mt-2">{data.tokenID}</div>
+		</div>
+	);
 }
 
 export default NFTCard;
