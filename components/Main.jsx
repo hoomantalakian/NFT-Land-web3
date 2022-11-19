3c89896842b1760807628de0179a6294168748a2
Hooman Talakian <hooman.talakian@gmail.com>
2022-11-20 00:58:55 +0330

    uuid is implemented (done)

--- components/Main.jsx
+++ components/Main.jsx
@@ -14,6 +14,7 @@ function Main({
 			{/* cards container */}
 			<cards className="mt-4 grid justify-center gap-5 md:grid-cols-2 lg:grid-cols-3 ">
 				{data.map((nft) => (
+					// uuid!
 					<NFTCard key={uuidv4()} data={nft}></NFTCard>
 				))}
 			</cards>
